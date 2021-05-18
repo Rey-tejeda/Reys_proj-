12e9847d0e24b025b13c6263df62391ccb3546c2
Rey <Rey@MSI>
2021-05-18 08:02:32 -0400

    Initial commit of project

---
+++ Reys_proj/src/Data.java
@@ -0,0 +1,9 @@
+
+
+public class Data {
+	private int data[] = {0, 4, 42};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
---
+++ Reys_proj/src/main.java
@@ -0,0 +1,22 @@
+public class main {
+
+
+
+	public static void displayGreeting() {
+		// TODO Auto-generated method stub
+		main data = new main();
+		
+		displayGreeting();
+		data.display();
+		}
+			
+	
+
+	private void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Happy Mary!");
+		System.out.println("This App Is For You!");
+	}
+
+	
+}
